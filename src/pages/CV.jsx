5dbed664f9b3df407021c70028722a4aa19fd68a
Vassja <v.misenko@web.de>
2025-11-07 18:25:41 +0100

    fixed some design issues and added cv preview

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,11 +1,21 @@
 import Header from "../components/Header";
+import "./CV.css";
+import cvPDF from "../assets/cv/CV - Vassilij Misenko.pdf"
+import cvImage from "../assets/cv/CV - Vassilij Misenko.jpg"
 
 export default function CV() {
   return (
     <div>
       <Header />
-      <div style={{ padding: "2rem", maxWidth: 900, margin: "0 auto" }}>
-        <h1>CV</h1>
+      <div className="cv-container">
+        <h1>My CV</h1>
+      </div>
+      <div>
+         <img 
+          src={cvImage} 
+          alt="CV Preview"
+          className="cv-preview"
+        />
       </div>
     </div>
   );
